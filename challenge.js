c34f0186639b7bfdc7bef15d9d460cdaa8187ca0
Jonathan Biro <jonnybrx@gmail.com>
2019-01-09 15:45:50 -0500

    ummm

--- challenge.js
+++ challenge.js
@@ -1,22 +1,42 @@
-
 let counter = document.getElementById("counter")
 let decrement = document.getElementById("-")
 let increment = document.getElementById("+")
 let like = document.getElementById("<3")
 let pause = document.getElementById("pause")
 
-setInterval(function(){
-  counter.innerText = parseInt(counter.innerText) + 1;
-}, 1000)
+var playing = !0
 
-decrement.addEventListener('click', function(){
+timer = function() {
+  return setInterval(function() {
+    counter.innerText = parseInt(counter.innerText) + 1
+  }, 1000)
+}
+interval = timer()
+
+
+decrement.addEventListener('click', function() {
   counter.innerText = parseInt(counter.innerText) - 1;
 })
 
-increment.addEventListener('click', function(){
+increment.addEventListener('click', function() {
   counter.innerText = parseInt(counter.innerText) + 1;
 })
 
-like.addEventListener('click', function(){
+like.addEventListener('click', function() {
 
+})
+
+// pause.addEventListener('click', function(){
+//   counter.innerText = parseInt(counter.innerText)
+// })
+
+
+
+
+
+pause.addEventListener("click", function() {
+
+  playing ? (playing = !1, clearInterval(interval), this.innerText = "resume") : (playing = !0, interval = timer(), this.innerText = "pause"), [].concat(_toConsumableArray(document.getElementsByTagName("button"))).forEach(function(a) {
+    "pause" !== document.getElementById("counter").id && (document.getElementById("counter").disabled = !playing)
+  })
 })
